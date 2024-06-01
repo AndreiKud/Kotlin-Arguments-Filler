b392e8a4cbcb91eccffbff48f1e2f5a603172aa3
Andrei Kudryavtsev <andreikudrya1995@gmail.com>
2024-06-01 17:07:30 +0500

    Empty project

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
+    id("org.jetbrains.intellij.platform") version "2.0.0-beta5"
+}
+
+group = "me.andreikud"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name = "Kotlin Arguments Filler"
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2023.2.6")
+
+        bundledPlugin("com.intellij.java") // ?
+
+        pluginVerifier()
+        zipSigner()
+        instrumentationTools()
+
+        testFramework(TestFrameworkType.Platform.JUnit4)
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("242.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
